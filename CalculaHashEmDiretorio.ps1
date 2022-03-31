7656b839aa2877cf99a1cdebc2a2f6a32c83a77c
diogo.nogueira <85765889+diogonogueiratotvs@users.noreply.github.com>
2022-03-30 22:57:23 -0300

    parte2

--- CalculaHashEmDiretorio.ps1
+++ CalculaHashEmDiretorio.ps1
@@ -1,14 +1,14 @@
 param($diretorio)
-
-$diretorio = "C:\Users\Diogo\OneDrive\DevOps\Powershell\files\"
-
+Import-Module .\ShaFiles.ps1
 $ErrorActionPreference = "Stop"
 
-. .\ShaFiles.PS1
+#$diretorio = 
+
+
 
 $arquivos = Get-ChildItem $diretorio -File
 
 foreach ($item in $arquivos) {
     $hashItem = Get-FileSHA1 $item
-    Write-Host "O Hast do arquivo $item é $hashItem !" #asd
+    Write-Host "O Hash do arquivo $item é $hashItem !" #asd
 }
--- ShaFiles.ps1
+++ ShaFiles.ps1
@@ -7,10 +7,21 @@ function Get-FileSHA1($filePath) {
     $hash = $sha1.ComputeHash($fileBytes)
 
     $prettyHashSB = New-Object System.Text.StringBuilder
+
     foreach ($byte in $hash) {
         $hexaNotation = $byte.ToString("X2")
-        $prettyHashSB.Append($hexaNotation)
+        $null = $prettyHashSB.Append($hexaNotation)
     }
 
-    Write-Host $hash -BackgroundColor RED -ForegroundColor Yellow #teste
+    $prettyHashSB.ToString()
 }
+
+# $arquivo = "C:\Users\Diogo\OneDrive\DevOps\alura_powershell_curso2\ShaFiles.ps1"
+# $hashDoArquivo = Get-FileSHA1 $arquivo
+
+# Write-Host "O hash do arquivo $arquivo eh $hashDoArquivo" -BackgroundColor Red -ForegroundColor Yellow
+
+
+
+
+
