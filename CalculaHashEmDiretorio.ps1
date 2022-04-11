26c2be9ea7243a7e5fbee7270448442bac4d71aa
diogo.nogueira <85765889+diogonogueiratotvs@users.noreply.github.com>
2022-04-11 16:27:12 -0300

    modulo 2

--- CalculaHashEmDiretorio.ps1
+++ CalculaHashEmDiretorio.ps1
@@ -1,14 +1,45 @@
-param($diretorio)
-Import-Module .\ShaFiles.ps1
-$ErrorActionPreference = "Stop"
+# Função  para  obtermos o hash do arquivo localizado em $filePath.
+function Get-FileSHA1($filePath) {
 
-#$diretorio = 
+    # Nossa primeira tarefa é verificar se o usuário  desta  função
+    # está utilizando o argumento $filePath ou se ele  deseja  usar
+    # o pipeline. Para tal, basta verificar se a várivel  $filePath
+    # é Não Igual (not equals, ou NE) a nulo  -  representado  pela 
+    # variável automática $null
 
+    if($filePath -ne $null) {
+        $fileContent = Get-Content $filePath
+        $fileBytes = [System.Text.Encoding]::UTF8.GetBytes($fileContent)
 
+        $sha1 = New-Object System.Security.Cryptography.SHA1Managed
 
-$arquivos = Get-ChildItem $diretorio -File
+        $hash = $sha1.ComputeHash($fileBytes)
 
-foreach ($item in $arquivos) {
-    $hashItem = Get-FileSHA1 $item
-    Write-Host "O Hash do arquivo $item é $hashItem !"
+        $prettyHashSB = New-Object System.Text.StringBuilder
+
+        foreach ($byte in $hash) {
+            $hexaNotation = $byte.ToString("X2")
+            $prettyHashSB.Append($hexaNotation) > $null
+        }
+
+        $prettyHashSB.ToString()
+    } else {
+        foreach ($item in $input) {
+            $fileContent = Get-Content $item
+            $fileBytes = [System.Text.Encoding]::UTF8.GetBytes($fileContent)
+
+            $sha1 = New-Object System.Security.Cryptography.SHA1Managed
+
+            $hash = $sha1.ComputeHash($fileBytes)
+
+            $prettyHashSB = New-Object System.Text.StringBuilder
+
+            foreach ($byte in $hash) {
+                $hexaNotation = $byte.ToString("X2")
+                $prettyHashSB.Append($hexaNotation) > $null
+            }
+
+            $prettyHashSB.ToString()
+        }
+    }
 }
