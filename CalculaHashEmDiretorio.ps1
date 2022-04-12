10d58d93d06892559cd0d0ca751de1ba5f3b98a1
diogo.nogueira <85765889+diogonogueiratotvs@users.noreply.github.com>
2022-04-12 09:36:11 -0300

    new commit

--- CalculaHashEmDiretorio.ps1
+++ CalculaHashEmDiretorio.ps1
@@ -1,45 +1,53 @@
 # Função  para  obtermos o hash do arquivo localizado em $filePath.
-function Get-FileSHA1($filePath) {
+function Get-FileSHA1 {
+    # Bloco de parâmetros. Aqui utilizamos o atributo Parameter
+    # para adornar características ao argumento $filePath.
+    param(
+        [Parameter(
+            # A propriedade ValueFromPipeline indica que este valor
+            # pode ser preenchido pelo pipeline.
+            ValueFromPipeline = $true,
+            # A propriedade ValueFromPipelineByPropertyName indica
+            # que o PowerShell pode enviar a esta função apenas  o
+            # valor da propriedade indicada, ao  invés  do  objeto
+            # inteiro.
+            ValueFromPipelineByPropertyName = "FullName",
+            # Usando a propriedade Mandatory, indicamos  que  este
+            # argumento é obrigatório.
+            Mandatory = $true
+        )]
+        [String] $filePath
+    )
 
-    # Nossa primeira tarefa é verificar se o usuário  desta  função
-    # está utilizando o argumento $filePath ou se ele  deseja  usar
-    # o pipeline. Para tal, basta verificar se a várivel  $filePath
-    # é Não Igual (not equals, ou NE) a nulo  -  representado  pela 
-    # variável automática $null
+    # Bloco de início, onde carregaremos os recursos utilizados
+    # por todos os itens recebidos pelo pipeline!
+    begin {
+        $sha1 = New-Object System.Security.Cryptography.SHA1Managed
+        $prettyHashSB = New-Object System.Text.StringBuilder
+    }
 
-    if($filePath -ne $null) {
+    # Bloco de processamento. Aqui serão executados os comandos
+    # a seguir para todos os itens de nosso pipeline.
+    process {
         $fileContent = Get-Content $filePath
         $fileBytes = [System.Text.Encoding]::UTF8.GetBytes($fileContent)
 
-        $sha1 = New-Object System.Security.Cryptography.SHA1Managed
-
         $hash = $sha1.ComputeHash($fileBytes)
-
-        $prettyHashSB = New-Object System.Text.StringBuilder
 
         foreach ($byte in $hash) {
             $hexaNotation = $byte.ToString("X2")
             $prettyHashSB.Append($hexaNotation) > $null
         }
 
-        $prettyHashSB.ToString()
-    } else {
-        foreach ($item in $input) {
-            $fileContent = Get-Content $item
-            $fileBytes = [System.Text.Encoding]::UTF8.GetBytes($fileContent)
+        $prettyHashSB.ToString() 
+        $prettyHashSB.Clear() > $null
+    }
 
-            $sha1 = New-Object System.Security.Cryptography.SHA1Managed
-
-            $hash = $sha1.ComputeHash($fileBytes)
-
-            $prettyHashSB = New-Object System.Text.StringBuilder
-
-            foreach ($byte in $hash) {
-                $hexaNotation = $byte.ToString("X2")
-                $prettyHashSB.Append($hexaNotation) > $null
-            }
-
-            $prettyHashSB.ToString()
-        }
+    # Bloco de fim, onde devemos liberar os recursos criados
+    # durante a execução de nossa função.
+    # No caso, precisamos invocar o método Dispose do objeto
+    # IDisposable SHA1Managed.
+    end {
+        $sha1.Dispose()
     }
 }
